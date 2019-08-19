512f17a4dbaabc720ce7a09ea12ff63dbeaaf4cc
Dirk Weil <dirk.weil@gedoplan.de>
2019-08-19 14:59:29 +0200

    Demo für MP Config ergänzt

---
+++ microprofile-config/src/main/java/de/gedoplan/showcase/RestApplication.java
@@ -0,0 +1,8 @@
+package de.gedoplan.showcase;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/")
+public class RestApplication extends Application {
+}
