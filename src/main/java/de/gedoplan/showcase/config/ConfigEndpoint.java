b4421727ef13669cdaf540aa518a74ea0189d309
Dirk Weil <dirk.weil@gedoplan.de>
2021-08-08 17:47:14 +0200

    Demos für Config Profiles und Config Expressions ergänzt

--- src/main/java/de/gedoplan/showcase/config/ConfigEndpoint.java
+++ src/main/java/de/gedoplan/showcase/config/ConfigEndpoint.java
@@ -6,12 +6,15 @@ import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 import org.eclipse.microprofile.config.ConfigProvider;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 @Path("config")
 @ApplicationScoped
+@Produces(MediaType.TEXT_PLAIN)
 public class ConfigEndpoint {
 
   // Demo of simple config api usage reading a value (normally) set by a system property
@@ -47,6 +50,42 @@ public class ConfigEndpoint {
   @GET
   public String getHello() {
     return "The answer to the ultimate question of life, the universe and everything is " + this.answer;
+  }
+
+  // Demo of using config profiles
+  @Inject
+  @ConfigProperty(name = "mp.config.profile", defaultValue = "none")
+  String profile;
+
+  @Inject
+  @ConfigProperty(name = "quarkus.profile")
+  Optional<String> quarkusProfile;
+
+  @Inject
+  @ConfigProperty(name = "database")
+  String database;
+
+  @Path("profile")
+  @GET
+  public CharSequence getProfileInfo() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("In profile ").append(this.profile);
+    if (this.quarkusProfile.isPresent()) {
+      sb.append(" (with Quarkus profile ").append(this.quarkusProfile.get()).append(")");
+    }
+    sb.append(" database is ").append(this.database);
+    return sb;
+  }
+
+  // Demo of using config profiles
+  @Inject
+  @ConfigProperty(name = "server.url")
+  String serverUrl;
+
+  @Path("expression")
+  @GET
+  public String getExpressionValue() {
+    return this.serverUrl;
   }
 
 }
